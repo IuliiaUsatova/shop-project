6156ca0cba481e376e2ce8e62d15e34af9137c0a
Misslee43 <millenium4363@gmail.com>
2023-01-25 09:59:13 +0300

    drop down

--- project shop/index.js
+++ project shop/index.js
@@ -1,95 +1,142 @@
 const header = {
-    headers: {
-        'Access-Control-Allow-Methods': 'GET,POST,DELETE,PUT',
-        'Access-Control-Allow-Origin': '*'
-    }
-}
+  headers: {
+    "Access-Control-Allow-Methods": "GET,POST,DELETE,PUT",
+    "Access-Control-Allow-Origin": "*",
+  },
+};
 
 window.onload = () => {
-    const clickIcon = document.getElementsByClassName("content-showerIcon");
-    const categoryItem = document.getElementsByClassName("category__hidden__inner__item");
-    const colorLetter = document.getElementsByClassName("category-list_letter");
-    const hiddenBlock = document.getElementsByClassName("category__hidden__content");
-    const searchTitle = document.getElementsByClassName('search__title')[0];
-    const itemImages = document.getElementsByClassName('item__image');
-    const headerBlockTitle = document.getElementsByClassName('header-block_title');
+  const clickIcon = document.getElementsByClassName("content-showerIcon");
+  const categoryItem = document.getElementsByClassName("category__hidden__inner__item");
+  const colorLetter = document.getElementsByClassName("category-list_letter");
+  const hiddenBlock = document.getElementsByClassName("category__hidden__content");
+  const searchTitle = document.getElementsByClassName("search__title")[0];
+  const itemImages = document.getElementsByClassName("item__image");
+  const headerBlockTitle = document.getElementsByClassName("header-block_title");
+  const searchInput = document.getElementsByClassName("search__input")[0];
+  const hiddenDropdown = document.getElementsByClassName("search__dropdown")[0];
+  const dropdownTexts = document.getElementsByClassName("search__dropdown__text");
 
-    for (let i = 0; i < clickIcon.length; i++) {
-        clickIcon[i].addEventListener("click", () => {
-            clickIcon[i].classList.toggle("rotateArrow");
-            hiddenBlock[i].classList.toggle("show");
-            colorLetter[i].classList.toggle("clickColor");  
-        })
+  //contains - если есть такой класс то возвращаем true, есни нет - false
+  //toggle - если есть класс, то он его добавит или удалит
+//   searchInput.addEventListener('focus', () => {
+//     // если есть hidden то мы убираем его при нажатии
+//     if (hiddenDropdown.classList.contains('hidden')) {
+//       hiddenDropdown.classList.remove('hidden');
+//     }
+//   });
+
+//   for (const text of dropdownTexts) {
+//     text.addEventListener('click', () => {
+//       //меняется категория с помощью нижней строки кода
+//       searchTitle.innerText = `All items for category ${text.innerText}`;
+//       hiddenDropdown.classList.add('hidden');
+//     });
+//   }
+
+
+
+
+
+  searchInput.addEventListener('focus', () => {
+    if (hiddenDropdown.classList.contains('hidden')) {
+        hiddenDropdown.classList.remove('hidden')
     }
+})
 
-    for (let i = 0; i < categoryItem.length; i++) {
-        categoryItem[i].addEventListener('click', () => {
-            showItemsByCategory(categoryItem[i], itemImages)
-            searchTitle.innerText = `All items for category ${categoryItem[i].innerText}`
-        })
-    }
-    for (const header of headerBlockTitle) {
-        const text = header.innerText
-        header.addEventListener('click', () => {
-            location.assign(`${text}.html`);
-        })
-    }
-    // CORS
-    // Обменивание ресурсов между сайтами. Безопастное обменивание.
-};
-//Про промисы:
-// Promise, fetch(), statuses of Promise, methods Promise
-// Заказ - запрос
-// Подарок - данные в обертке
+for (const text of dropdownTexts) {
+    text.addEventListener('click', () => {
+        searchTitle.innerText = `All items for category: ${text.innerText}`
+        hiddenDropdown.classList.add('hidden')
+    })
+}
 
-// fetch () -> 2 arguments; 1 -> стринг (URL), 2 -> настройки запроса : Promise<any>
-// Pending, Fulfilled, Rejected, Settled
-// Response, .status -> 100, 200, 300, 400, 500
-// then(), catch(), finally(), all(), race()
-// then() -> 2 arguments; 1 -> callback success, 2 -> callback failed
-// catch() -> 1 argument; callback no success
-// finally() -> argument; callback anytime
-// all() -> 1 argument; array of requests
-// race() -> 1 argument; array of requests -> 1 Promise
-// 1 req -> small data, 2 req -> medium data, 3 req -> hard data
 
-// где classList - это класс, который содержит все массивы  
 
-//Про асинхронность
-// Asynchronous functions (async / await)
 
-async function showItemsByCategory (categoryItem, itemImages) {
 
+  for (let i = 0; i < clickIcon.length; i++) {
+    clickIcon[i].addEventListener("click", () => {
+      clickIcon[i].classList.toggle("rotateArrow");
+      hiddenBlock[i].classList.toggle("show");
+      colorLetter[i].classList.toggle("clickColor");
+    });
+  }
+
+  for (let i = 0; i < categoryItem.length; i++) {
+    categoryItem[i].addEventListener("click", () => {
+      showItemsByCategory(categoryItem[i], itemImages);
+      searchTitle.innerText = `All items for category ${categoryItem[i].innerText}`;
+    });
+  }
+  for (const header of headerBlockTitle) {
+    const text = header.innerText;
+    header.addEventListener("click", () => {
+      location.assign(`${text}.html`);
+    });
+  }
+
+  // CORS
+  // Обменивание ресурсов между сайтами. Безопастное обменивание.
+
+  //Про промисы:
+  // Promise, fetch(), statuses of Promise, methods Promise
+  // Заказ - запрос
+  // Подарок - данные в обертке
+
+  // fetch () -> 2 arguments; 1 -> стринг (URL), 2 -> настройки запроса : Promise<any>
+  // Pending, Fulfilled, Rejected, Settled
+  // Response, .status -> 100, 200, 300, 400, 500
+  // then(), catch(), finally(), all(), race()
+  // then() -> 2 arguments; 1 -> callback success, 2 -> callback failed
+  // catch() -> 1 argument; callback no success
+  // finally() -> argument; callback anytime
+  // all() -> 1 argument; array of requests
+  // race() -> 1 argument; array of requests -> 1 Promise
+  // 1 req -> small data, 2 req -> medium data, 3 req -> hard data
+
+  // где classList - это класс, который содержит все массивы
+
+  //Про асинхронность
+  // Asynchronous functions (async / await)
+
+  async function showItemsByCategory(categoryItem, itemImages) {
     // Promise -> then(функция при успешном запросе, при неуспешном), catch(функция), finally(функция)
     // NO -> fetch().then().catch()
     // YES -> fetch() async / await
 
-    const res = await (await fetch('https://fakestoreapi.com/products')).json();
+    const res = await (await fetch("https://fakestoreapi.com/products")).json();
     // 200, 300, 400, 500
     // 200 -> успешный запрос
     // 300 -> сайт либо переместили куда-то или удален
     // 400 -> ошибка со стороны пользователя
     // 500 -> ошибка со стороны сервера
-    
+
     // Pending Promise (Pending, Fulfilled, Rejected, Settled)
-    const selectedCategory = categoryItem.innerText === 'For men' ? "men's clothing" : "women's clothing"
-    const filteredItems = res.filter(item => item.category === selectedCategory)
+    const selectedCategory =
+      categoryItem.innerText === "For men"
+        ? "men's clothing"
+        : "women's clothing";
+    const filteredItems = res.filter(
+      (item) => item.category === selectedCategory
+    );
     filteredItems.forEach((element, index) => {
-        itemImages[index].setAttribute('src', element.image);
-    })
-}
+      itemImages[index].setAttribute("src", element.image);
+    });
+  }
 
-// 2 function -> обработку (не асинхронные)
-// 1 function -> отвечает за сохранение данных каждые 10 секунд (async)
-// 1 function -> отвечает за обновление данных каждые 10 секунд (async)
+  // 2 function -> обработку (не асинхронные)
+  // 1 function -> отвечает за сохранение данных каждые 10 секунд (async)
+  // 1 function -> отвечает за обновление данных каждые 10 секунд (async)
 
-// NO ASYNC
-// 1 -> 0sec
-// 2 -> 0sec
+  // NO ASYNC
+  // 1 -> 0sec
+  // 2 -> 0sec
 
-// ASYNC
-// 1 -> 0sec
-// 2 -> 0sec
+  // ASYNC
+  // 1 -> 0sec
+  // 2 -> 0sec
 
-// 10 секунд -> 20 sec
-// 0 секунд -> 10 sec
+  // 10 секунд -> 20 sec
+  // 0 секунд -> 10 sec
+};
